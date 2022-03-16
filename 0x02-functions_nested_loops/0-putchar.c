82ba23e9acdf8347970cb1e51aa87d810b0cd768
bisrattamene <tamene_bisrat@yahoo.com>
2022-03-16 03:27:03 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,12 +6,14 @@
  */
 int main(void)
 {
-char *ch = "_putchar";
-while (*ch)
-{
-_putchar(*ch);
-ch++;
-}
+_putchar('-');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+-putchar('r');
 _putchar('\n');
 return (0);
 }
