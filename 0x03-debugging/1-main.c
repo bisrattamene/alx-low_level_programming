138fbc09522b301996a5feba2be1f9ac29243ebe
bisrattamene <tamene_bisrat@yahoo.com>
2022-03-17 11:27:17 -0700

    My main commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,11 +12,10 @@ printf("Infinite loop incoming :(\n");
 
 i = 0;
 
-/*
-*while (i < 10)
+/** while (i < 10)
 *{
-*putchar(i);
-}
+* putchar(i);
+*}
 */
 printf("infinite loop avoided! \\o/\n");
 return (0);
