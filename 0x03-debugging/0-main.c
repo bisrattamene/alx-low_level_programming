6a2a11b70266688580764d73740062cabb83979d
bisrattamene <tamene_bisrat@yahoo.com>
2022-03-17 08:02:26 -0700

    My main commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,8 @@
 #include "main.h"
-#include <stdio.h>
 /**
- *main - main
- *Return: 0
+ * main - tests function that prints if integer is positive or negative
+ *
+ * Return: 0
  */
 int main(void)
 {
